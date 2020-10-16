05e1471fd0276472901ff03a16e9ee3849b5f881
shixiongfei <jenson.shixf@gmail.com>
2020-10-17 00:36:44 +0800

    Cross platform

--- kbhit.c
+++ kbhit.c
@@ -10,19 +10,26 @@
  */
 
 #include "kbhit.h"
+#ifdef _WIN32
+#include <conio.h>
+#else /* _WIN32 */
 #include <fcntl.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <termios.h>
 #include <unistd.h>
+#endif /* _WIN32 */
 
+#ifndef _WIN32
 static struct termios orig_termios;
 
 static void reset_terminal_mode(void) {
   tcsetattr(STDIN_FILENO, TCSANOW, &orig_termios);
 }
+#endif /* _WIN32 */
 
 void setup_terminal_mode(void) {
+#ifndef _WIN32
   struct termios new_termios;
 
   tcgetattr(STDIN_FILENO, &orig_termios);
@@ -31,9 +38,13 @@ void setup_terminal_mode(void) {
   tcsetattr(STDIN_FILENO, TCSANOW, &new_termios);
 
   atexit(reset_terminal_mode);
+#endif /* _WIN32 */
 }
 
 int kbhit(void) {
+#ifdef _WIN32
+  return _kbhit();
+#else /* _WIN32 */
   int oldf, ch;
 
   oldf = fcntl(STDIN_FILENO, F_GETFL, 0);
@@ -49,6 +60,13 @@ int kbhit(void) {
   }
 
   return 0;
+#endif /* _WIN32 */
 }
 
-int getch(void) { return getchar(); }
+int getch(void) {
+#ifdef _WIN32
+  return _getch();
+#else /* _WIN32 */
+  return getchar();
+#endif /* _WIN32 */
+}
